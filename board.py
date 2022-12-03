9311ed6cab3dfa6b1edf9e0248774e83e150449c
AyGeeEm <aagarciamarin@gmail.com>
2022-12-02 23:55:26 -0500

    board class structure added

---
+++ board.py
@@ -0,0 +1,39 @@
+"""Code edited by Alex Garcia-Marin on 12/2/22"""
+
+
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        pass
+
+    def draw(self):
+        pass
+
+    def select(self):
+        pass
+
+    def click(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self, value):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
